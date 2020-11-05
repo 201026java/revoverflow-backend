ce4370341caa8c4efc1733a0461b7008c0b34e91
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-11-05 17:09:26 -0500

    Added data sql file back into Answer service

---
+++ revoverflow-backend - MICROSERVICE/AnswerService/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into answers (user_id, question_id, content, creation_date, edit_date)
+VALUES(14, 1, 'answer content 1', '2012-12-13', NULL), 
+(14, 1, 'answer content 2', '2012-12-13', NULL),
+(14, 1, 'answer content 3', '2012-12-13', NULL), 
+(13, 2, 'answer content 4', '2012-12-13', NULL),  
+(13, 2, 'answer content 5', '2012-12-13', NULL);
