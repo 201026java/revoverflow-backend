ae6e5fadddb5937dd90d02e5365c3b3563cb63d4
Mark Alsip <mark.alsip@comcast.net>
2020-11-05 14:22:49 -0700

    implemented question service by thomas, added messaging layout

---
+++ revoverflow-backend - MICROSERVICE/QuestionService/src/main/java/com/revature/models/Answer.java
@@ -0,0 +1,133 @@
+package com.revature.models;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
+
+public class Answer {
+
+	private int id;
+	private int userId;
+	private int questionId;
+	private String content;
+	private LocalDateTime creationDate;
+	private LocalDateTime editDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public LocalDateTime getEditDate() {
+		return editDate;
+	}
+
+	public void setEditDate(LocalDateTime editDate) {
+		this.editDate = editDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((creationDate == null) ? 0 : creationDate.hashCode());
+		result = prime * result + ((editDate == null) ? 0 : editDate.hashCode());
+		result = prime * result + id;
+		result = prime * result + questionId;
+		result = prime * result + userId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (creationDate == null) {
+			if (other.creationDate != null)
+				return false;
+		} else if (!creationDate.equals(other.creationDate))
+			return false;
+		if (editDate == null) {
+			if (other.editDate != null)
+				return false;
+		} else if (!editDate.equals(other.editDate))
+			return false;
+		if (id != other.id)
+			return false;
+		if (questionId != other.questionId)
+			return false;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [id=" + id + ", userId=" + userId + ", questionId=" + questionId + ", content=" + content
+				+ ", creationDate=" + creationDate + ", editDate=" + editDate + "]";
+	}
+
+	public Answer(int id, int userId, int questionId,
+			@NotBlank(message = "Content must have a string value") String content, LocalDateTime creationDate,
+			LocalDateTime editDate) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.questionId = questionId;
+		this.content = content;
+		this.creationDate = creationDate;
+		this.editDate = editDate;
+	}
+
+	public Answer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
